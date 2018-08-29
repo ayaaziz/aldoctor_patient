508378ccc779afb32a9cfed6f74963a8d6df59e9
samar <samarnasef80@gmail.com>
2018-08-29 14:28:16 +0200

    ...

--- src/providers/helper/helper.ts
+++ src/providers/helper/helper.ts
@@ -14,6 +14,7 @@ export class HelperProvider {
   public lang_direction ='rtl';
   public currentLang ='ar';
   //public serviceUrl: string = "http://itrootsdemos.com/aldoctor/public/";
+  
   //public serviceUrl: string = "http://aldoctor-app.com/aldoctortest/public/";
   
   public serviceUrl: string = "http://aldoctor-app.com/aldoctor/public/";
