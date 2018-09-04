726c6884be6b7c02ab85841213b5f0227e11a246
samar <samarnasef80@gmail.com>
2018-09-04 15:54:06 +0200

    change rate to rated value

--- src/pages/orderhistory/orderhistory.ts
+++ src/pages/orderhistory/orderhistory.ts
@@ -144,8 +144,8 @@ export class OrderhistoryPage {
           
               // this.orderobject.name = serviceProfile.name;
               this.orderobject.profile_pic = serviceProfile.profile_pic;
-              this.orderobject.rate = serviceProfile.rate;
-              // this.orderobject.rate = ordersData[j].ratedvalue;
+              //this.orderobject.rate = serviceProfile.rate;
+              this.orderobject.rate = ordersData[j].ratedvalue;
               
               if(ordersData[j].service_id && ordersData[j].service_id == "3" )
               {
