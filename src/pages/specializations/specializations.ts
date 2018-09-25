47ee802f09f93afe98ef21203f4be4a3f3b58599
samar <samarnasef80@gmail.com>
2018-09-25 13:33:10 +0200

    help icon in specialization

--- src/pages/specializations/specializations.ts
+++ src/pages/specializations/specializations.ts
@@ -33,7 +33,12 @@ export class SpecializationsPage {
   tostClass ;
   searchValue;
   showLoading=true;
-
+  helpersArr=[];
+  helpers2 = true;
+  helpers3 = true;
+  phone;
+  phone2;
+  phone3;
   
   constructor(public helper:HelperProvider,public navCtrl: NavController,
      public navParams: NavParams,public storage: Storage,
@@ -61,6 +66,7 @@ export class SpecializationsPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SpecializationsPage');
+    this.initializeHelper();
     this.initializeSpecializations();
   }
 
@@ -288,6 +294,65 @@ console.log("sp item search val ",val);
     //
   }
 
-
+  initializeHelper(){
+    this.service.getuserProfile(this.accessToken).subscribe(
+      resp=>{
+        console.log("resp from getuserProfile ",resp);
+        console.log("city_id",JSON.parse(JSON.stringify(resp)).extraInfo.city_id);
+        this.service.getHelperTelephones(JSON.parse(JSON.stringify(resp)).extraInfo.city_id,this.accessToken).subscribe(
+          resp=>{
+            console.log("resp from getHelperTelephones",resp);
+            this.helpersArr = JSON.parse(JSON.stringify(resp));
+      
+      
+             //top bottom right left 
+          if(this.helpersArr.length == 1)
+          {
+            this.helpersArr[0].fabDir="right";
+            this.phone = this.helpersArr[0].value;
+          }
+          else if (this.helpersArr.length == 2)
+          {
+            this.helpersArr[0].fabDir="right";
+            this.helpersArr[1].fabDir="left ";
+            this.helpers2 = false;
+            this.helpers3 = true;
+            this.phone = this.helpersArr[0].value;
+            this.phone2 = this.helpersArr[1].value;
+      
+          }
+          else if (this.helpersArr.length == 3)
+          {
+            this.helpersArr[0].fabDir="right";
+            this.helpersArr[1].fabDir="left ";
+            this.helpersArr[2].fabDir="top";
+            this.helpers2 = false;
+            this.helpers3 = false;
+            this.phone = this.helpersArr[0].value;
+            this.phone2 = this.helpersArr[1].value;
+            this.phone3 = this.helpersArr[2].value;
+          }else{
+            this.helpersArr[0].fabDir="right";
+            this.helpersArr[1].fabDir="left ";
+            this.helpersArr[2].fabDir="top";
+            this.helpers2 = false;
+            this.helpers3 = false;
+            this.phone = this.helpersArr[0].value;
+            this.phone2 = this.helpersArr[1].value;
+            this.phone3 = this.helpersArr[2].value;
+          }
+      
+          },
+          err=>{
+            console.log("errfrom getHelperTelephones",err);
+          });
+   
+          
+      }
+      ,err=>{
+        console.log("can't getuserProfile ",err);
+      });
+    
+  }
 
 }
